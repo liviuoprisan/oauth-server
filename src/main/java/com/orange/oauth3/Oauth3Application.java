36a788070dd3f185760759804592c7d8c1dddd27
liviu oprisan <liviuoprisan@orange.com>
2020-01-14 12:10:41 +0200

    first commit with basic ouath server and index page

---
+++ src/main/java/com/orange/oauth3/Oauth3Application.java
@@ -0,0 +1,15 @@
+package com.orange.oauth3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@EnableAuthorizationServer
+@SpringBootApplication
+public class Oauth3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/orange/oauth3/Oauth3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.oauth3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
